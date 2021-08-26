5554bc5e0dadcb9d6ebe1838410aa33ec5a79135
Sujeet Bhujbal <sujeet.bhujbal@aciworldwide.com>
2021-08-26 17:40:17 +0530

    Add project files.

---
+++ FileData/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using ThirdPartyTools;
+using static System.Console;
+
+namespace FileData
+{
+    public static class Program
+    {
+        private static readonly string[] VersionDetails = {"-v", "--v", "/v", "--version"};
+        private static readonly string[] FilesDetails = {"-s", "--s", "/s", "--size"};
+
+        public static void Main(string[] args)
+        {
+            ReadVersion(args);
+            ReadSize(args);
+            ReadLine();
+        }
+
+        public static string ReadVersion(string[] args)
+        {
+            if (args.Length <= 0)
+                WriteLine("Input is null, Please provide input");
+            else
+            {
+                var fs = new FileDetails();
+                if (VersionDetails != null && VersionDetails.Contains(args[0]))
+                {
+                    WriteLine(fs.Version(args[0]));
+                    return fs.Version(args[0]);
+                }
+            }
+            return string.Empty;
+        }
+
+
+        public static int ReadSize(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                WriteLine("Input is null, Please provide input");
+            }
+            else
+            {
+                var fs = new FileDetails();
+                if (FilesDetails != null && FilesDetails.Contains(args[0]))
+                {
+                    WriteLine(fs.Size(args[0]));
+                    return fs.Size(args[0]);
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ FileDataTestProject/FileDataTest.cs
@@ -0,0 +1,46 @@
+﻿using FileData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileDataTestProject
+{
+    [TestClass]
+    public class FileDataTest
+    {
+        [TestMethod]
+        public void GivenFileVersion_processFileVersion_ReturnFileVersion()
+        {
+            var inputParameters = new[] {"-v", " C:/test.txt"};
+
+            var output = Program.ReadVersion(inputParameters);
+            Assert.IsTrue(!string.IsNullOrEmpty(output));
+        }
+
+
+        [TestMethod]
+        public void GivenFileInvalidVersionName_DoesNotProcessFileVersion_ReturnFileVersion()
+        {
+            var inputParameters = new[] {"1", " C:/test.txt"};
+
+            var output = Program.ReadVersion(inputParameters);
+            Assert.IsTrue(string.IsNullOrEmpty(output));
+        }
+
+
+        [TestMethod]
+        public void GivenFileSize_processFileSize_ReturnFileSize()
+        {
+            var inputParameters = new[] {"-s", " C:/test.txt"};
+            var output = Program.ReadSize(inputParameters);
+            Assert.IsTrue(output > 0);
+        }
+
+
+        [TestMethod]
+        public void GivenInvalidFileSizeName_DoesNotProcessFileSize_ReturnFileSize()
+        {
+            var inputParameters = new[] {"-suj", " C:/test.txt"};
+            var output = Program.ReadSize(inputParameters);
+            Assert.IsTrue(output == 0);
+        }
+    }
+}
